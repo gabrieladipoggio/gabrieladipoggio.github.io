7554b61c0e691989383dad64ca67b186a99a5f16
gabrieladipoggio <gabrieladipoggio@hotmail.com>
2021-07-04 17:31:38 -0300

    First draft

---
+++ script.js
@@ -0,0 +1,8 @@
+var arrow = document.querySelector('.arrow')
+
+  arrow.addEventListener('mouseover', function () {
+     this.classList.toggle('animate__animated')
+     this.classList.toggle('animate__bounce')
+    }
+);
+          
